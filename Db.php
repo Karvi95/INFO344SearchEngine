87e09872b5a692f0897c258fd4f718bee67f1d41
Arvindram Krishnamoorthy <karvi90@yahoo.com>
2017-01-15 17:15:58 -0800

    Saving

---
+++ Db.php
@@ -0,0 +1,47 @@
+<?php 
+
+require_once('BBallPlayer.php');
+
+class Db {
+	private $hostname, $username, $password, $dbname, $dbh;
+	
+	function __construct() {
+	}
+
+	function search($searchterm) {
+		$results = array();
+
+		if (strlen($searchterm) >= 1 && $searchterm !== ' ') {
+		
+			$hostname = 'ards.c8vxadxemz8c.us-west-2.rds.amazonaws.com';
+			$username = 'info344user';
+			$password = 'password';
+			$dbname = 'BasketballPlayerDB';
+			
+			$dbh = new PDO("mysql:host=$hostname;dbname=$dbname",$username,$password);
+			$dbh -> exec("SET CHARACTER SET utf8");
+			$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			$searchterm = $dbh->quote($searchterm);
+
+			$sql = "SELECT * 
+					FROM `NBAInfo` 
+					WHERE CONCAT( levenshtein(`EntireName`, {$searchterm}) < 2, 
+								  levenshtein(`Suffix`, {$searchterm}) < 2 , 
+								  levenshtein(`LName`, {$searchterm}) < 2 ,
+								  levenshtein(`MName`, {$searchterm}) < 2 ,  
+								  levenshtein(`FName`, {$searchterm}) < 2
+								)";
+			$sth = $dbh->query($sql);
+			$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,'\BBallPlayer',
+				array('EntireName', 'FName', 'MName', 'LName', 'Suffix', 'Team','GP','Min','FG_M','FG_A','FG_Pct','ThreePT_M','ThreePT_A','ThreePT_Pct','FT_M','FT_A','FT_Pct','Rebounds_Off','Rebounds_Def','Rebounds_Tot','Ast','TO','Stl','Blk','PF','PPG'));
+			$results = $sth->fetchAll();
+
+			var_dump($results);
+		}
+	}
+
+	function kill() {
+		$dbh = null;
+	}
+}
